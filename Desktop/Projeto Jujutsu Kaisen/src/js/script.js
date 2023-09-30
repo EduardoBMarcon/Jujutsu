fac30e8dbf2a6ef204e9546a9ba36e1b4cb8a57d
Eduardo <eduardobmb2017@gmail.com>
2023-09-30 16:28:58 -0300

    Primeiro commit

---
+++ Desktop/Projeto Jujutsu Kaisen/src/js/script.js
@@ -0,0 +1,16 @@
+const lista = ['gojo','getou','yuta','sukuna','nobara', 'toji']
+
+$('.option').on('click',function(event){
+    var nome = $(this).data('value');
+    removeAll()
+    addClasses(nome)
+});
+
+function removeAll(){
+    lista.forEach((nome) => $(`#${nome}`).addClass('display-none') & $(`#option-menu-${nome}`).removeClass('option-selected'));
+}
+
+function addClasses(nome){
+    $(`#${nome}`).removeClass('display-none')
+    $(`#option-menu-${nome}`).addClass('option-selected')
+}
